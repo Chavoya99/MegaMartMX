e6fecb62eb26101498ce074afc6275ff782f64d2
Chavoya99 <carlos.chavoya4676@alumnos.udg.mx>
2024-02-29 14:25:40 -0600

    Creando tabla de productos

--- database/migrations/2024_02_29_200058_create_productos_table.php
+++ database/migrations/2024_02_29_200058_create_productos_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('categoria');
+            $table->string('subCategoria');
+            $table->decimal('precio', 10, 2);
+            $table->string('codigoBarras');
             $table->timestamps();
         });
     }
