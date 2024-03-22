a7c5759795efdb991446267ccb20e107c7be3853
Mally Samira Hernandez Martinez <mally.hernandez2861@alumnos.udg.mx>
2024-03-21 22:33:20 -0600

    Diseño Index
    Cambio de diseño en Index con la plantilla implementada.

--- resources/views/proveedores/proveedorIndex.blade.php
+++ resources/views/proveedores/proveedorIndex.blade.php
@@ -1,44 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de proveedores</title>
-</head>
-<body>
-<a class="btn btn-primary" href="{{route('proveedor.create')}}">Agregar proveedor</a>
-<h1>Tabla proveedores</h1>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Direccion</th>
-                <th>Correo</th>
-                <th>Telefono</th>
-                <th>Estado</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($proveedores as  $proveedor)
+<x-mi-layout titulo="Lista de proveedores">
+    <a class="btn btn-primary" href="{{route('proveedor.create')}}">Agregar proveedor</a><br><br>
+        <table class="table">
+            <thead>
                 <tr>
-                    <td>{{$proveedor->nombre}}</td>
-                    <td>{{$proveedor->direccion}}</td>
-                    <td>{{$proveedor->correo}}</td>
-                    <td>{{$proveedor->telefono}}</td>
-                    <td>{{$proveedor->estado}}</td>
-                    <td>
-                        <a href="{{route('proveedor.show', $proveedor)}}">Detalle</a> |
-                        <a href="{{route('proveedor.edit', $proveedor)}}">Editar</a> |
-                        <form onsubmit="return confirm('Se eliminará el siguiente registro, ¿desea continuar?')" action="{{route('proveedor.destroy', $proveedor)}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <input type="submit" value="Eliminar">
-                        </form>                       
-                    </td>
-                </tr> 
-            @endforeach
-        </tbody>
-    </table>
-</body>
-</html>
+                    <th>Nombre</th>
+                    <th>Direccion</th>
+                    <th>Correo</th>
+                    <th>Telefono</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($proveedores as  $proveedor)
+                    <tr>
+                        <td>{{$proveedor->nombre}}</td>
+                        <td>{{$proveedor->direccion}}</td>
+                        <td>{{$proveedor->correo}}</td>
+                        <td>{{$proveedor->telefono}}</td>
+                        <td>{{$proveedor->estado}}</td>
+                        <td>
+                            <a class="btn btn-primary" href="{{route('proveedor.show', $proveedor)}}"> <i class="fa fa-info-circle"></i>Detalles</a>
+                            <a class="btn btn-success" href="{{route('proveedor.edit', $proveedor)}}">&#x270E;Editar</a>
+                            <form onsubmit="return confirm('Se eliminará el siguiente registro, ¿Desea continuar?')" action="{{route('proveedor.destroy', $proveedor)}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <input class="btn btn-danger" type="submit" value="x Eliminar">
+                            </form>                       
+                        </td>
+                    </tr> 
+                @endforeach
+            </tbody>
+        </table>
+    </x-mi-layout>
