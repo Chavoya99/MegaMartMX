e2dfbf2ab06720115ff4615f0f90ad7bab38f00e
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2024-04-27 15:19:18 -0600

    Actualización catálogo de categorías, subcategorias y productos
    Se modificaron los seeders de categoria, subcategoria, proveedor y se añadió ProductoSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             CategoriaSeeder::class,
             SubcategoriaSeeder::class,
             ProveedorSeeder::class,
+            ProductoSeeder::class,
         ]);
     }
 }
--- database/seeders/SubcategoriaSeeder.php
+++ database/seeders/SubcategoriaSeeder.php
@@ -13,8 +13,12 @@ class SubcategoriaSeeder extends Seeder
      */
     public function run(): void
     {   
-        $subcategorias = ['Lácteos'=>1, 'Vinos y licores' =>1, 'Refrescos' => 1, 
-        'Muebles' => 2, 'Cereales' => 3, 'Frituras' => 3,'Videojuegos'=> 4];
+        $subcategorias = ['Vinos y licores' =>1, 'Refrescos' => 1, 'Agua' => 1,
+        'Muebles' => 2, 'Limpieza' => 2,
+        'Cereales' => 3, 'Frituras' => 3, 'Verduras' => 3, 'Frutas' => 3, 'Carnicería' => 3, 'Enlatados' => 3,
+        'Videojuegos'=> 4,
+        'Carnes Frías' => 5, 'Lácteos' => 5,
+        'Cuidado del cabello' => 6, 'Cuidado de la piel' => 6];
         foreach($subcategorias as $subcategoria => $valor){
             subcategoria::create([
                 'nombre' => $subcategoria,
