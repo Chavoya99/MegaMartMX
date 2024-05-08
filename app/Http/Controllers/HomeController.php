cfc633e53e86053f5fa9f90b1e3bfc16319b6d06
Mally Samira Hernandez Martinez <mally.hernandez2861@alumnos.udg.mx>
2024-05-08 05:15:39 -0600

    Carrito de compras, correcciones de vistas  y detalles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,59 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Categoria;
 use App\Models\Producto;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $productos = Producto::with(['categoria', 'subcategoria', 'proveedor', 'archivo'])->orderBy('nombre')->get();
-        return view('usuario.homeIndex', compact('productos'));
+        $productos = Producto::with(['categoria', 'subcategoria', 'proveedor', 'archivo'])->orderBy('nombre');
 
+        if ($request->has('categoria') && $request->categoria != 'all') {
+            $productos->where('categoria_id', $request->categoria);
+        }
+
+        $productos = $productos->get();
+        $categorias = Categoria::all();
+
+        return view('usuario.homeIndex', compact('productos', 'categorias'))->with('categoriaSeleccionada', $request->categoria ?? 'all');
     }
+
 
     public function show(Producto $producto)
     {
         return view('usuario.homeShow', compact('producto'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
