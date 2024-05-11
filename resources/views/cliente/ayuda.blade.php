b824be9ebdde14753fc623fcd65acfb7d620475e
Mally-Martinez <mally.hernandez2861@alumnos.udg.mx>
2024-05-11 16:23:11 -0600

    Creación de sección de ayuda y quienes somos

---
+++ resources/views/cliente/ayuda.blade.php
@@ -0,0 +1,12 @@
+<x-cliente-layout titulo="Ayuda">
+    <!-- Contenido de ayuda -->
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Aquí va tu contenido de ayuda -->
+                <h1>Bienvenido a la sección de ayuda</h1>
+                <p>Aquí encontrarás toda la información que necesitas.</p>
+            </div>
+        </div>
+    </div>
+</x-cliente-layout>
