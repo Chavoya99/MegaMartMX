16e6418d92a0230288979a43b4f3f2fe2d1abbd6
Chavoya99 <carlos.chavoya4676@alumnos.udg.mx>
2024-05-11 16:00:36 -0600

    modificación de cliente layout: diseño para botón de inicio de sesión

--- resources/views/components/cliente-layout.blade.php
+++ resources/views/components/cliente-layout.blade.php
@@ -319,11 +319,19 @@
                         </li>
 
                         <div class="topbar-divider d-none d-sm-block"></div>
+                        <div class="container-fluid">
                         @if(!Auth::check())
-                            <a href="{{route('login')}}">Iniciar sesión</a>   
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="btn btn-primary" href="{{route('login')}}">
+                                <i><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
+                                stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-in">
+                                <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/><polyline points="10 17 15 12 10 7"/>
+                                <line x1="15" x2="3" y1="12" y2="12"/></svg></i>
+                                Iniciar sesión</a>
+                        </li>
                         @endif
                              
-
                         <!-- Nav Item - User Information -->
                         @if (Auth::check())
                             <li class="nav-item dropdown no-arrow">
