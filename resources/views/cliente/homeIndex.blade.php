f332a98758c6c9970b4443baff21a70679c7fbaa
Mally-Martinez <mally.hernandez2861@alumnos.udg.mx>
2024-05-11 04:47:41 -0600

    Ventana de "guardar en favoritos" añadida en homeIndex

--- resources/views/cliente/homeIndex.blade.php
+++ resources/views/cliente/homeIndex.blade.php
@@ -55,41 +55,72 @@
         <div class="row">
             @if(count($productos) > 0)
                 @foreach ($productos as $producto)
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card h-100">
-                            <a href="{{ route('cliente.producto.show', $producto) }}">
-                                @if($producto->archivo)
-                                    <img class="card-img-top" src="{{ asset(\Storage::url($producto->archivo->ubicacion)) }}" alt="{{ $producto->nombre }}" style="max-height: 200px;"> 
-                                @else
-                                    <img class="card-img-top" src="ruta/a/imagen/por/defecto.jpg" alt="{{ $producto->nombre }}" style="max-height: 200px;"> <!-- Si no hay archivo asociado, muestra una imagen por defecto -->
-                                @endif
-                            </a>
-                            <div class="card-body">
-                                <h4 class="card-title">
-                                    <a href="{{ route('cliente.producto.show', $producto) }}">{{ $producto->nombre }}</a>
-                                </h4>
-                                <h5>${{ $producto->precio }}</h5>
-                                <p class="card-text">{{ $producto->descripcion }}</p>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-success">Envío gratis <i class="fas fa-bolt"></i></small>
-                                <small class="text-muted">a partir de $150 </small> 
-                                <small class="text-muted">Disponibles: {{ $producto->existencia }}</small>
-                                <div class="d-flex justify-content-between mt-2">
-                                    <form action="{{ route('carrito.agregar', ['id' => $producto->id]) }}" method="POST" id="agregarForm{{$producto->id}}" style="display: inline;">
-                                        @csrf
-                                        <button type="button" class="btn btn-success btn-sm" onclick="agregarAlCarrito({{$producto->id}})"><i class="fas fa-plus"></i> Agregar</button>
-                                    </form>
-                                    
-                                    <form action="{{route('cliente.nuevo_favorito', $producto)}}" method="POST" style="display: inline;">
-                                        @csrf
-                                        <button type="submit" class="btn btn-danger btn-sm"><i class="fas fa-heart"></i> Guardar</button>
-                                    </form>
-                                    
-                                </div>
+                <div class="col-lg-3 col-md-6 mb-4">
+                    <div class="card h-100">
+                        <a href="{{ route('cliente.producto.show', $producto) }}">
+                            @if($producto->archivo)
+                                <img class="card-img-top" src="{{ asset(\Storage::url($producto->archivo->ubicacion)) }}" alt="{{ $producto->nombre }}" style="max-height: 200px;"> 
+                            @else
+                                <img class="card-img-top" src="ruta/a/imagen/por/defecto.jpg" alt="{{ $producto->nombre }}" style="max-height: 200px;"> <!-- Si no hay archivo asociado, muestra una imagen por defecto -->
+                            @endif
+                        </a>
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                <a href="{{ route('cliente.producto.show', $producto) }}">{{ $producto->nombre }}</a>
+                            </h4>
+                            <h5>${{ $producto->precio }}</h5>
+                            <p class="card-text">{{ $producto->descripcion }}</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-success">Envío gratis <i class="fas fa-bolt"></i></small>
+                            <small class="text-muted">a partir de $150 </small> 
+                            <small class="text-muted">Disponibles: {{ $producto->existencia }}</small>
+                            <div class="d-flex justify-content-between mt-2">
+                                <form action="{{ route('carrito.agregar', ['id' => $producto->id]) }}" method="POST" id="agregarForm{{$producto->id}}" style="display: inline;">
+                                    @csrf
+                                    <button type="button" class="btn btn-success btn-sm" onclick="agregarAlCarrito({{$producto->id}})"><i class="fas fa-plus"></i> Agregar</button>
+                                </form>
+                                
+                                <form action="{{route('cliente.nuevo_favorito', $producto)}}" method="POST" id="guardarForm{{$producto->id}}" style="display: inline;">
+                                    @csrf
+                                    <button type="button" class="btn btn-danger btn-sm" onclick="guardarProducto({{$producto->id}})"><i class="fas fa-heart"></i> Guardar</button>
+                                </form>
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <!-- Ventana modal de guardar producto en favoritos -->
+                <div class="modal fade" id="guardarModal{{$producto->id}}" tabindex="-1" role="dialog" aria-labelledby="guardarModalLabel{{$producto->id}}" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="guardarModalLabel{{$producto->id}}">¡Que bien!</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body text-center">
+                                <p>El producto ha sido agregado a favoritos<br>¡Sigue asi!</p>
+                                <div class="container mt-3 mb-3 text-center">
+                                    <img src="{{ asset('img/guardar.png') }}" alt="Seguir comprando" style="max-width: 150px;">
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                
+                                <a href="{{ route('cliente.favoritos') }}" class="btn btn-danger">
+                                    <i class="fas fa-heart"></i> Ir a favoritos
+                                </a>
+                                <div class="ml-auto">
+                                    <button type="button" class="btn btn-warning text-dark" data-dismiss="modal">
+                                        Seguir comprando <i class="fas fa-arrow-right"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
                 @endforeach
             @else
                 <div class="col-md-12 text-center">
@@ -106,15 +137,15 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="mensajeModalLabel">Producto Agregado al Carrito</h5>
+                <h5 class="modal-title " id="mensajeModalLabel">¡Excelente!</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
-                <p>El producto ha sido agregado correctamente al carrito de compras.</p>
+            <div class="modal-body text-center">
+                <p>El producto ha sido agregado  al carrito de compras<br>¡Sigue asi!</p>
                 <div class="container mt-3 mb-3 text-center">
-                    <img src="{{ asset('img/seguir.png') }}" alt="Seguir comprando" style="max-width: 100px;">
+                    <img src="{{ asset('img/seguir.png') }}" alt="Seguir comprando" style="max-width: 150px;">
                 </div>
             </div>
             <div class="modal-footer">
@@ -134,17 +165,6 @@
 </div>
 
 <script>
-    function submitForm() {
-        document.getElementById("filtroForm").submit();
-    }
-
-    window.onload = function() {
-        var categoriaSeleccionada = "{{ $categoriaSeleccionada }}";
-        if (categoriaSeleccionada) {
-            document.getElementById("categoria").value = categoriaSeleccionada;
-        }
-    }
-
     function agregarAlCarrito(idProducto) {
         var form = document.getElementById("agregarForm" + idProducto);
         var formData = new FormData(form);
@@ -164,5 +184,30 @@
             console.error('Error:', error);
             alert("Hubo un error al agregar el producto al carrito");
         });
+    }
+
+    function guardarProducto(idProducto) {
+        var form = document.getElementById("guardarForm" + idProducto);
+        var formData = new FormData(form);
+
+        fetch(form.action, {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            if (response.ok) {
+                $('#guardarModal' + idProducto).modal('show'); 
+            } else {
+                alert("Hubo un error al guardar el producto en favoritos");
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert("Hubo un error al guardar el producto en favoritos");
+        });
+    }
+
+    function seguirComprando(idProducto) {
+        $('#guardarModal' + idProducto).modal('hide');
     }
 </script>
