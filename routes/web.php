0128aa9ace1d9c424b778fa05e702f32f0b19ccc
Chavoya99 <carlos.chavoya4676@alumnos.udg.mx>
2024-05-12 01:45:37 -0600

    Actualización de middleware auth

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,11 @@ Route::get('/', function () {
 });
 
 
-Route::middleware('auth')->group(function(){
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function(){
     
 
     Route::middleware(AdminMiddleware::class)->group(function(){
