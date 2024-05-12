644a0caa387bbb9383e036da8138eb921c692ff2
Mally-Martinez <mally.hernandez2861@alumnos.udg.mx>
2024-05-11 23:40:38 -0600

    Mejora visual de Confirmar compra

--- resources/views/cliente/confirmarCarrito.blade.php
+++ resources/views/cliente/confirmarCarrito.blade.php
@@ -25,10 +25,11 @@
                         }
                     @endphp
                     @if ($subtotal >= 150)
-                        <p style="color: green;">Tu pedido califica para envío GRATIS</p>
-                    @else
-                        <p style="color: grey;">Envío: $50</p>
-                    @endif
+                    <p style="color: green; font-size: 15px;">Tu pedido califica para envío GRATIS</p>
+                @else
+                    <p style="color: grey; font-size: 15px;">Envío: $50</p>
+                @endif
+                <hr>
                     <p>Subtotal ({{ $totalProductos }} productos): <br><strong style="font-size: 32px;">${{ number_format($subtotal, 2) }}</strong></p>
                     <p>Total: <br><strong style="font-size: 32px;">${{ number_format($total, 2) }}</strong></p>
                 </div>
@@ -70,120 +71,38 @@
                     </div>
                     
                     <hr>
-                    <h5 class="card-title">Detalles de entrega
-                        <span id="toggle-details" style="cursor: pointer; float: right;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
-                                <polyline points="6 9 12 15 18 9"></polyline>
-                            </svg>
-                        </span>
-                    </h5>
+                    <div class="mb-2 offset-md-0">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" name="recogerEnTienda" id="recogerEnTienda" checked>
+                            <label class="form-check-label" for="recogerEnTienda">
+                                <strong>Recoger en tienda:</strong> Blvd. Gral. Marcelino García Barragán 1421, Olímpica, 44430 Guadalajara, Jal. 
+                                <br><strong>Próximamente: </strong>entregas a domicilio
+                            </label>
+                        </div>
+                    </div>
+                    <div class="mb-2 offset-md-0">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="pago_efectivo" value="1" id="pagoEfectivo" checked>
+                            <label class="form-check-label" for="pagoEfectivo">
+                                <strong>Pago</strong> en efectivo <br><strong>Próximamente: </strong>pago con tarjetas de credito/debito
+                            </label>
+                        </div>
+                    </div>
+                    <div class="mb-2 offset-md-0">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" name="confirmarEnvioCorreo" id="confirmarEnvioCorreo" checked>
+                            <label class="form-check-label" for="confirmarEnvioCorreo"><strong>Enviar </strong>mi recibo de compra por correo electrónico</label>
+                        </div>
+                    </div>
                     <hr>
-                    <form action="{{ route('carrito.comprar', [$subtotal, $total, $envio]) }}" method="POST" id="form-detalles-entrega">
-                        @csrf
-                        <div id="details-section" style="display: none;">
-                            <div class="mb-3">
-                                <label for="calle" class="form-label">Calle:</label>
-                                <input type="text" class="form-control" id="calle" name="calle" placeholder="Ingresa el nombre de la calle">
-                            </div>
-                            <div class="row">
-                                <div class="col-md-4 mb-3">
-                                    <label for="numero" class="form-label">Número:</label>
-                                    <input type="text" class="form-control" id="numero" name="numero" placeholder="Número">
-                                </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="codigo_postal" class="form-label">C.P:</label>
-                                    <input type="text" class="form-control" id="codigo_postal" name="codigo_postal" placeholder="Código Postal">
-                                </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="colonia" class="form-label">Colonia:</label>
-                                    <input type="text" class="form-control" id="colonia" name="colonia" placeholder="Colonia">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 mb-3">
-                                    <label for="ciudad" class="form-label">Ciudad:</label>
-                                    <select class="form-select" id="ciudad" name="ciudad">
-                                        <option value="">Selecciona una ciudad</option>
-                                        <option value="Ciudad de México">CDMX</option>
-                                        <option value="Guadalajara">Guadalajara</option>
-                                        <option value="Monterrey">Monterrey</option>
-                                        <option value="Puebla">Puebla</option>
-                                        <option value="Mérida">Mérida</option>
-                                        <option value="Cancún">Cancún</option>
-                                    </select>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="estado" class="form-label">Estado:</label>
-                                    <select class="form-select" id="estado" name="estado">
-                                        <option value="">Selecciona un estado</option>
-                                        <option value="Aguascalientes">Aguascalientes</option>
-                                        <option value="Baja California">Baja California</option>
-                                        <option value="Baja California Sur">Baja California Sur</option>
-                                        <option value="Campeche">Campeche</option>
-                                        <option value="Chiapas">Chiapas</option>
-                                        <option value="Chihuahua">Chihuahua</option>
-                                        <option value="Coahuila">Coahuila</option>
-                                        <option value="Colima">Colima</option>
-                                        <option value="Durango">Durango</option>
-                                        <option value="Guanajuato">Guanajuato</option>
-                                        <option value="Guerrero">Guerrero</option>
-                                        <option value="Hidalgo">Hidalgo</option>
-                                        <option value="Jalisco">Jalisco</option>
-                                        <option value="México">México</option>
-                                        <option value="Michoacán">Michoacán</option>
-                                        <option value="Morelos">Morelos</option>
-                                        <option value="Nayarit">Nayarit</option>
-                                        <option value="Nuevo León">Nuevo León</option>
-                                        <option value="Oaxaca">Oaxaca</option>
-                                        <option value="Puebla">Puebla</option>
-                                        <option value="Querétaro">Querétaro</option>
-                                        <option value="Quintana Roo">Quintana Roo</option>
-                                        <option value="San Luis Potosí">San Luis Potosí</option>
-                                        <option value="Sinaloa">Sinaloa</option>
-                                        <option value="Sonora">Sonora</option>
-                                        <option value="Tabasco">Tabasco</option>
-                                        <option value="Tamaulipas">Tamaulipas</option>
-                                        <option value="Tlaxcala">Tlaxcala</option>
-                                        <option value="Veracruz">Veracruz</option>
-                                        <option value="Yucatán">Yucatán</option>
-                                        <option value="Zacatecas">Zacatecas</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <hr>
-                        </div>
-                        <div class="mb-2 offset-md-0">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="pago_efectivo" value="1" id="pagoEfectivo" checked>
-                                <label class="form-check-label" for="pagoEfectivo">
-                                    Pago: efectivo
-                                </label>
-                            </div>
-                        </div>
-                        <div class="mb-2 offset-md-0">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="pago_efectivo" value="1" id="pagoEfectivo" checked>
-                                <label class="form-check-label" for="pagoEfectivo">
-                                    Descargar recibo de compra
-                                </label>
-                            </div>
-                        </div>
-                        <div class="mb-2 offset-md-0">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" name="confirmarEnvioCorreo" id="confirmarEnvioCorreo" checked>
-                                <label class="form-check-label" for="confirmarEnvioCorreo">Enviar mi recibo de compra por correo electrónico</label>
-                            </div>
-                        </div>
-                        <hr>
-                        <button type="submit" class="btn btn-warning text-dark mt-0">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-dollar-sign">
-                                <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/>
-                                <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8"/>
-                                <path d="M12 18V6"/>
-                            </svg>
-                            Confirmar compra
-                        </button>
-                    </form>
+                    <button type="submit" class="btn btn-warning text-dark mt-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-dollar-sign">
+                            <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/>
+                            <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8"/>
+                            <path d="M12 18V6"/>
+                        </svg>
+                        Confirmar compra
+                    </button>
                 </div>
             </div>
         </div>
@@ -200,31 +119,6 @@
                         productsSection.style.display = 'none';
                         toggleProducts.innerHTML = 'Productos <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather" style="float: right;"><polyline points="6 9 12 15 18 9"></polyline></svg>';
                     }
-                });
-        
-                const toggleDetails = document.getElementById('toggle-details');
-                const detailsSection = document.getElementById('details-section');
-        
-                toggleDetails.addEventListener('click', function() {
-                    if (detailsSection.style.display === 'none') {
-                        detailsSection.style.display = 'block';
-                        toggleDetails.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>';
-                    } else {
-                        detailsSection.style.display = 'none';
-                        toggleDetails.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>';
-                    }
-                });
-            });
-
-            $(document).ready(function(){
-                $('input, select').each(function() {
-                    $(this).change(function(){
-                        if($(this).val() !== '') {
-                            $(this).addClass('is-valid');
-                        } else {
-                            $(this).removeClass('is-valid');
-                        }
-                    });
                 });
             });
         </script>
